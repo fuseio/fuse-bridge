b94e2df56691e412a37ef88e34049f22c27018cd
Lior Rabin <lior@fuse.io>
2019-12-31 12:10:40 +0200

    updated flatten script

--- native-to-erc20/contracts/scripts/flatten.sh
+++ native-to-erc20/contracts/scripts/flatten.sh
@@ -8,8 +8,9 @@ mkdir flats
 
 ./node_modules/.bin/truffle-flattener contracts/upgradeable_contracts/native_to_erc20/HomeBridgeNativeToErc.sol > flats/HomeBridgeNativeToErc_flat.sol
 ./node_modules/.bin/truffle-flattener contracts/upgradeable_contracts/native_to_erc20/ForeignBridgeNativeToErc.sol > flats/ForeignBridgeNativeToErc_flat.sol
+./node_modules/.bin/truffle-flattener contracts/ERC677BridgeToken.sol > flats/ERC677BridgeToken_flat.sol
 
 ./node_modules/.bin/truffle-flattener contracts/upgradeable_contracts/BridgeValidators.sol > flats/BridgeValidators_flat.sol
-./node_modules/.bin/truffle-flattener contracts/upgradeable_contracts/native_to_erc20/ForeignBridgeValidators.sol > flats/ForeignBridgeValidators.sol
+./node_modules/.bin/truffle-flattener contracts/upgradeable_contracts/native_to_erc20/ForeignBridgeValidators.sol > flats/ForeignBridgeValidators_flat.sol
 
 ./node_modules/.bin/truffle-flattener contracts/upgradeability/EternalStorageProxy.sol > flats/EternalStorageProxy_flat.sol
