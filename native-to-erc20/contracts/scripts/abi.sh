8a2402867ac95001a98e5c71ea1bf545ebebb212
Leon Prouger <leonprou@gmail.com>
2020-11-30 17:43:35 +0200

    Fixing scripts

--- native-to-erc20/contracts/scripts/abi.sh
+++ native-to-erc20/contracts/scripts/abi.sh
@@ -10,6 +10,5 @@ mkdir abis
 ./node_modules/node-jq/bin/jq '.abi' build/contracts/ForeignBridgeNativeToErc.json > abis/ForeignBridgeNativeToErc.abi.json
 
 ./node_modules/node-jq/bin/jq '.abi' build/contracts/BridgeValidators.json > abis/BridgeValidators.abi.json
-./node_modules/node-jq/bin/jq '.abi' build/contracts/ForeignBridgeValidators.json > abis/ForeignBridgeValidators.abi.json
 
 ./node_modules/node-jq/bin/jq '.abi' build/contracts/EternalStorageProxy.json > abis/EternalStorageProxy.abi.json
