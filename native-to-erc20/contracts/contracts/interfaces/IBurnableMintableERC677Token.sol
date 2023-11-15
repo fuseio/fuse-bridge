0551172b6cad241bfae790c675ef9b37a2f2d0d3
Mayank Mittal <msquare.pvt21@gmail.com>
2023-11-15 23:44:38 +0530

    Update Contracts

--- native-to-erc20/contracts/contracts/interfaces/IBurnableMintableERC677Token.sol
+++ native-to-erc20/contracts/contracts/interfaces/IBurnableMintableERC677Token.sol
@@ -6,4 +6,5 @@ contract IBurnableMintableERC677Token is ERC677 {
     function mint(address, uint256) public returns (bool);
     function burn(uint256 _value) public;
     function claimTokens(address _token, address _to) public;
+    function addMinter(address account) public;
 }
